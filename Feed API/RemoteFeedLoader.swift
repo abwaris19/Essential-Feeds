361691f23970cb44bb03a79919210261c5ee0fc7
abdulwariskhan <hellokhan.ch@gmail.com>
2024-04-08 17:13:26 +0200

    Add more test assertions for different HTTP status code

--- Feed API/RemoteFeedLoader.swift
+++ Feed API/RemoteFeedLoader.swift
@@ -11,7 +11,7 @@ import Foundation
 
 public protocol HTTPClient {
    
-    func get(from url: URL, completion: @escaping (Error) -> Void)
+    func get(from url: URL, completion: @escaping (Error?, HTTPURLResponse?) -> Void)
     
 }
 
@@ -22,6 +22,7 @@ public final class RemoteFeedLoader {
    
     public enum Error: Swift.Error {
         case connectivity
+        case invalidData
     }
     
     public  init(url: URL, client: HTTPClient) {
@@ -30,9 +31,17 @@ public final class RemoteFeedLoader {
     }
     
     public func load( completion: @escaping (Error) -> Void ) {
-        client.get(from: url) {
-            error in
-            completion(.connectivity)
+        client.get(from: url) { error,
+            response in
+            
+            if response != nil
+            {
+                completion(.invalidData)
+            } else
+            {
+                completion(.connectivity)
+            }
+          
             
         }
     }
